8d9664383844ddc1a1531b24d220a398b895a3e0
Eric Mogendi <mogendieric19@gmail.com>
2024-03-07 12:31:35 +0300

    complete dark mode functionality

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import userReducer from './user/userSlice';
+import themeReducer from './theme/themeSlice';
 import { persistReducer, persistStore } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
 const rootReducer = combineReducers({
 	user: userReducer,
+	theme: themeReducer,
 });
 
 const persistConfig = {
@@ -18,7 +20,7 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 export const store = configureStore({
 	reducer: persistedReducer,
 	middleware: (getDefaultMiddleware) =>
-			getDefaultMiddleware({ serializableCheck: false }),
+		getDefaultMiddleware({ serializableCheck: false }),
 });
 
 export const persistor = persistStore(store);

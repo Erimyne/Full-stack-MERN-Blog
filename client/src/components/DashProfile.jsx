f0d7d03387a2e8fd3e4f86ac5764ec420d77ad62
Eric Mogendi <mogendieric19@gmail.com>
2024-03-12 18:22:06 +0300

    complete update user profile page functionality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -10,6 +10,12 @@ import {
 import { app } from '../firebase';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
+import {
+	updateStart,
+	updateSuccess,
+	updateFailure,
+} from '../redux/user/userSlice';
+import { useDispatch } from 'react-redux';
 
 export default function DashProfile() {
 	const { currentUser } = useSelector((state) => state.user);
@@ -17,7 +23,13 @@ export default function DashProfile() {
 	const [imageFileUrl, setImageFileUrl] = useState(null);
 	const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
 	const [imageFileUploadError, setImageFileUploadError] = useState(null);
+	const [imageFileUploading, setImageFileUploading] = useState(false);
+	const [updateUserSuccess, setUpdateUserSuccess] = useState(null);
+	const [updateUserError, setUpdateUserError] = useState(null);
+
+	const [formdata, setFormData] = useState({});
 	const filePickerRef = useRef();
+	const dispatch = useDispatch();
 
 	const handleImageChange = (e) => {
 		const file = e.target.files[0];
@@ -42,6 +54,7 @@ export default function DashProfile() {
 		// 	  }
 		// 	}
 		//   }
+		setImageFileUploading(true);
 		setImageFileUploadError(null);
 		const storage = getStorage(app);
 		const fileName = new Date().getTime() + imageFile.name;
@@ -61,18 +74,61 @@ export default function DashProfile() {
 				setImageFileUploadProgress(null);
 				setImageFile(null);
 				setImageFileUrl(null);
+				setImageFileUploading(false);
 			},
 			() => {
 				getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
 					setImageFileUrl(downloadURL);
+					setFormData({ ...formdata, profilePicture: downloadURL });
+					setImageFileUploading(false);
 				});
 			}
 		);
 	};
+
+	const handleChange = (e) => {
+		setFormData({ ...formdata, [e.target.id]: e.target.value });
+	};
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		setUpdateUserError(null);
+		setUpdateUserSuccess(null);
+		if (Object.keys(formdata).length === 0) {
+			setUpdateUserError('No changes made');
+			return;
+		}
+		if (imageFileUploading) {
+			setUpdateUserError('Please wait for the image to upload');
+			return;
+		}
+		try {
+			dispatch(updateStart());
+			const res = await fetch(`/api/user/update/${currentUser._id}`, {
+				method: 'PUT',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify(formdata),
+			});
+
+			const data = await res.json();
+			if (!res.ok) {
+				dispatch(updateFailure(data.message));
+				setUpdateUserError(data.message);
+			} else {
+				dispatch(updateSuccess(data));
+				setUpdateUserSuccess('User profile updated successfully');
+			}
+		} catch (error) {
+			dispatch(updateFailure(error.message));
+			setUpdateUserError(error.message);
+		}
+	};
 	return (
 		<div className="max-w-lg mx-auto p-3 w-full">
 			<h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
-			<form className="flex flex-col gap-4">
+			<form onSubmit={handleSubmit} className="flex flex-col gap-4">
 				<input
 					type="file"
 					accept="image/*"
@@ -122,14 +178,21 @@ export default function DashProfile() {
 					id="username"
 					placeholder="Username"
 					defaultValue={currentUser.username}
+					onChange={handleChange}
 				/>
 				<TextInput
 					type="email"
 					id="email"
 					placeholder="Email"
 					defaultValue={currentUser.email}
+					onChange={handleChange}
 				/>
-				<TextInput type="password" id="password" placeholder="Password" />
+				<TextInput
+					type="password"
+					id="password"
+					placeholder="Password"
+					onChange={handleChange}
+				/>
 				<Button type="submit" gradientDuoTone="purpleToBlue" outline>
 					Update
 				</Button>
@@ -138,6 +201,16 @@ export default function DashProfile() {
 				<span className="cursor-pointer">Delete Account</span>
 				<span className="cursor-pointer">Sign Out</span>
 			</div>
+			{updateUserSuccess && (
+				<Alert color="success" className="mt-5">
+					{updateUserSuccess}
+				</Alert>
+			)}
+			{updateUserError && (
+				<Alert color="failure" className="mt-5">
+					{updateUserError}
+				</Alert>
+			)}
 		</div>
 	);
 }

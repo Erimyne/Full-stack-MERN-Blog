e05218ee5a0689c02a810acaa35fd895219adb71
Eric Mogendi <mogendieric19@gmail.com>
2024-03-14 12:55:16 +0300

    add delete user api route

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -18,9 +18,9 @@ function Header() {
 				className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
 			>
 				<span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-					Erimyne's
+					Erimyne
 				</span>
-				Blog
+				Sports
 			</Link>
 			<form>
 				<TextInput

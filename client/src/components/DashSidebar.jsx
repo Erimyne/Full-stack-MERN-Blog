c9af139431a505ce4ccc5be24c0329ac68240cfb
Eric Mogendi <mogendieric19@gmail.com>
2024-03-25 15:42:01 +0300

    add comments section to the admin dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,6 +1,6 @@
 import { Sidebar } from 'flowbite-react';
 import { useEffect, useState } from 'react';
-import { HiArrowSmRight, HiDocumentText, HiOutlineUserGroup, HiUser } from 'react-icons/hi';
+import { HiAnnotation, HiArrowSmRight, HiDocumentText, HiOutlineUserGroup, HiUser } from 'react-icons/hi';
 import { Link, useLocation } from 'react-router-dom';
 import { signoutSuccess } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
@@ -61,19 +61,32 @@ export default function DashSidebar() {
 						
 					)}
 
-
-					{currentUser.isAdmin && (
-						<Link to="/dashboard?tab=users">
-							<Sidebar.Item
-								active={tab === 'users'}
-								icon={HiOutlineUserGroup}
-								as="div"
-							>
-								Users
-							</Sidebar.Item>
-						</Link>
-						
-					)}
+					<>
+						{currentUser.isAdmin && (
+							<Link to="/dashboard?tab=users">
+								<Sidebar.Item
+									active={tab === 'users'}
+									icon={HiOutlineUserGroup}
+									as="div"
+								>
+									Users
+								</Sidebar.Item>
+							</Link>
+							
+						)}
+						{currentUser.isAdmin && (
+							<Link to="/dashboard?tab=comments">
+								<Sidebar.Item
+									active={tab === 'comments'}
+									icon={HiAnnotation}
+									as="div"
+								>
+									Comments
+								</Sidebar.Item>
+							</Link>
+							
+						)}
+					</>	
 
 					<Sidebar.Item
 						icon={HiArrowSmRight}

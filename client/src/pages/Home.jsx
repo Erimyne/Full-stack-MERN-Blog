5f83b9ef1f99a06434516b30871594877bac491d
Eric Mogendi <mogendieric19@gmail.com>
2024-03-25 19:20:53 +0300

    modification of the home page and the post card

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -15,7 +15,7 @@ export default function Home() {
   }, []);
   return (
     <div>
-      <div className='flex flex-col gap-6 mx-auto p-28 pl-5 max-w-6xl'>
+      <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto'>
         <h1 className='text-3xl font-bold lg:text-6xl'>Welcome to my Blog</h1>
         <p className='text-gray-500 text-sm'>
           Here you'll find a variety of articles and tutorials on topics such as
